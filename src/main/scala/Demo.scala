9e865f876b638dd00c176dcf9927456b2e14ad68
Taniya Gamage <taniyasawani@gmail.com>
2023-07-24 21:59:58 +0530

    Initial commit

---
+++ src/main/scala/Demo.scala
@@ -0,0 +1,45 @@
+import scala.io.StdIn.readLine
+
+object Demo {
+
+  def encryption(text: String, shift: Int): String = {
+    val encryptedText = new StringBuilder()
+
+    for (i <- 0 to text.length-1) {
+      var encryptedChar = ' '
+      if(text.charAt(i) != ' '){
+        encryptedChar = (text.charAt(i) + shift).toChar
+      }
+      encryptedText.append(encryptedChar)
+    }
+    encryptedText.toString()
+  }
+
+  def decryption(text: String, shift: Int): String = {
+    val encryptedText = new StringBuilder()
+
+    for (i <- 0 to text.length - 1) {
+      var encryptedChar = ' '
+      if (text.charAt(i) != ' ') {
+        encryptedChar = (text.charAt(i) - shift).toChar
+      }
+      encryptedText.append(encryptedChar)
+    }
+    encryptedText.toString()
+  }
+
+
+  def main(args : Array[String]): Unit = {
+    print("Enter plaintext : ")
+    var text = readLine()
+
+    print("Enter shift count : ")
+    var shift = readLine().toInt
+
+    var encryptedText = encryption(text,shift)
+    println(encryptedText)
+
+    println(decryption(encryptedText,shift))
+
+  }
+}
